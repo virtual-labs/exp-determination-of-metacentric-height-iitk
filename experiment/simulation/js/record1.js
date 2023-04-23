b1b6d0769da7a4fbbbd5f7ecf88b860b3aea956e
sheetal <sheetal.singh458@gmail.com>
2023-04-23 17:45:00 +0530

    updated folder

---
+++ experiment/simulation/js/record1.js
@@ -0,0 +1,42 @@
+function datasheet1() {
+    document.getElementById("sno").innerHTML = "1";
+    document.getElementById("y2").innerHTML = "21";
+    document.getElementById("wc").innerHTML = "10250";
+    document.getElementById("w1").innerHTML = "900";
+    document.getElementById("degree").innerHTML = "20";
+    document.getElementById("Zl").innerHTML = "2.54";
+    document.getElementById("h").innerHTML = "11.78";
+}
+
+function datasheet2() {
+    document.getElementById("sno").innerHTML = "1";
+    document.getElementById("y2").innerHTML = "21";
+    document.getElementById("wc").innerHTML = "10250";
+    document.getElementById("w1").innerHTML = "900";
+    document.getElementById("degree").innerHTML = "10";
+    document.getElementById("Zl").innerHTML = "5.08";
+    document.getElementById("h").innerHTML = "2.32";
+}
+
+function datasheet3() {
+    document.getElementById("sno").innerHTML = "1";
+    document.getElementById("y2").innerHTML = "21";
+    document.getElementById("wc").innerHTML = "10250";
+    document.getElementById("w1").innerHTML = "900";
+    document.getElementById("degree").innerHTML = "20";
+    document.getElementById("Zl").innerHTML = "7.62";
+    document.getElementById("h").innerHTML = "3.31";
+}
+
+function datasheet4() {
+    document.getElementById("sno").innerHTML = "1";
+    document.getElementById("y2").innerHTML = "21";
+    document.getElementById("wc").innerHTML = "10250";
+    document.getElementById("w1").innerHTML = "900";
+    document.getElementById("degree").innerHTML = "10";
+    document.getElementById("Zl").innerHTML = "10.16";
+    document.getElementById("h").innerHTML = "4.57";
+}
+
+
+
